c9d5f9f75eea0a50d7e03dc93ab95f84e28b4337
Chidalu Nsobundu <kidstarro22@gmail.com>
2022-10-01 21:14:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+let transit=""
+let number2= 0
+let number1 = 0
+let counterCheck = 0
+let output= document.getElementById("ans")
+
+for(let i=0; i<10; i++){
+    let buttonId= document.getElementById("btn-" + i)
+    buttonId.addEventListener( 'click', function(){
+        transit= transit+i
+        output.textContent=transit
+    })
+}
+ function operate(op){
+    number1= Number(transit)
+    counterCheck+=1
+    transit=""
+    operand=op
+ }
+function ceOp(){location.reload()}
+function addOp(){operate("add")}
+function subOp(){operate("sub")}
+function divOp(){operate("div")}
+function mulOp(){operate("mul")}
+
+function equalOp(){
+    if (counterCheck===1){
+        number2=Number(transit)
+        switch(operand){
+            case "add":
+                answer= number1 + number2
+                transit=""
+                counterCheck=0
+            break
+            case "sub":
+                answer= number1-number2
+                transit=""
+                counterCheck=0
+            break
+            case "div":
+                answer= number1/number2
+                answer.toFixed(5)
+                transit=""
+                counterCheck=0
+            break
+            case "mul":
+                answer= number1*number2
+                transit=""
+                counterCheck=0
+            break
+        }
+        
+    }
+    output.textContent=answer
+}
+
+
